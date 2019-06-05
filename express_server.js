5d090d560478273d588188ecc6739f5418709116
Steven Wing <steven.wing82@gmail.com>
2019-06-05 01:28:07 +0000

    added database so server restart reset database data

--- express_server.js
+++ express_server.js
@@ -1,3 +1,4 @@
+var fs = require("fs");
 var express = require("express");
 var app = express();
 var PORT = 8080; // default port 8080
@@ -6,12 +7,52 @@ const bodyParser = require("body-parser");
 app.use(bodyParser.urlencoded({ extended: true }));
 app.set("view engine", "ejs");
 
-var urlDatabase = {
-    "b2xVn2": "http://www.lighthouselabs.ca",
-    "9sm5xK": "http://www.google.com"
-};
+function generateRandomString() {
+    let charSet = '0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz'
+    let randomNum = () =>
+        Math.floor(Math.random() * charSet.length);
 
-app.get("/", function(req, res) {
+    let randomStr = ""
+
+    for (let i = 0; i < 6; i++) {
+        randomStr += charSet.charAt(randomNum());
+    }
+
+    return randomStr;
+
+}
+let urlDatabase = {};
+var buf = new Buffer.alloc(1024);
+
+fs.open('./database.backup', 'r+', function(err, fd) {
+    if (err) {
+        return console.error(err);
+    }
+    fs.read(fd, buf, 0, buf.length, 0, function(err, bytes) {
+        if (err) {
+            console.log(err);
+        }
+        let db = (buf.slice(0, bytes).toString());
+        urlDatabase = JSON.parse(db);
+    });
+});
+setTimeout(function() {
+    console.log("Database Loaded")
+}, 1000);
+
+const backupDatabase = () => {
+    fs.writeFile('database.backup', JSON.stringify(urlDatabase), function(err) {
+        if (err) {
+            return console.error(err);
+        }
+    })
+};
+// {
+//     "b2xVn2": "http://www.lighthouselabs.ca",
+//     "9sm5xK": "http://www.google.com"
+// };
+
+app.get("/", (req, res) => {
     res.send("Hello!");
 });
 
@@ -21,10 +62,10 @@ app.listen(PORT, () => {
 app.get("/urls/new", (req, res) => {
     res.render("urls_new");
 });
-app.get("/urls.json", function(req, res) {
+app.get("/urls.json", (req, res) => {
     res.json(urlDatabase);
-})
-app.get("/urls", function(req, res) {
+});
+app.get("/urls", (req, res) => {
     let templateVars = { urls: urlDatabase };
     res.render("urls_index", templateVars);
 });
@@ -34,14 +75,20 @@ app.get("/urls/:shortURL", (req, res) => {
     res.render("urls_show", templateVars);
 });
 
-app.get("/hello", function(req, res) {
-    res.send("<html><body>Hello <b>World</b></body></html>\n");
+app.get("/u/:shortURL", (req, res) => {
+    let shortURL = req.params.shortURL;
+    if (urlDatabase[shortURL]) {
+        res.redirect(urlDatabase[shortURL]);
+    } else {
+        res.redirect('/urls');
+    }
+
 });
 app.post("/urls", (req, res) => {
-    console.log(req.body); // Log the POST request body to the console
-    res.send("Ok"); // Respond with 'Ok' (we will replace this)
+    console.log(req.body.longURL); // Log the POST request body to the console
+    let newShort = generateRandomString();
+    urlDatabase[newShort] = req.body.longURL;
+    backupDatabase();
+    console.log("Database Backup Complete");
+    res.redirect(`urls/${newShort}`);
 });
-
-function generateRandomString() {
-
-}
